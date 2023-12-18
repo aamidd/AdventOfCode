de6118c3774c3d4e1af270f38dc149515e9f1b16
Amid Sanai <sanaiamid@gmail.com>
2023-12-18 22:57:05 +0330

    solved day1

---
+++ src/day1/Main.java
@@ -0,0 +1,34 @@
+package day1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int sum =
+                Files.lines(Paths.get("input"))
+                        .mapToInt(Main::firstAndLast)
+                        .sum();
+
+        System.out.println(sum);
+    }
+
+    private static int firstAndLast(String line) {
+        String digits = line.replaceAll("one", "one1one")
+                .replaceAll("two", "two2two")
+                .replaceAll("three", "three3three")
+                .replaceAll("four", "four4four")
+                .replaceAll("five", "five5five")
+                .replaceAll("six", "six6six")
+                .replaceAll("seven", "seven7seven")
+                .replaceAll("eight", "eight8eight")
+                .replaceAll("nine", "nine9nine")
+                .replaceAll("\\D+", "");
+
+        String firstAndLast = String.valueOf(digits.charAt(0)) + digits.charAt(digits.length() - 1);
+        System.out.printf("%s -> %s = %s%n", line, digits, firstAndLast);
+        return Integer.parseInt(firstAndLast);
+    }
+}
