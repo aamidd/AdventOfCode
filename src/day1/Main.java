4f10ff277714593bce8b7777c6efc88880db409f
Amid Sanai <sanaiamid@gmail.com>
2023-12-19 07:54:58 +0330

    added the answer to part one of the question

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -2,21 +2,28 @@ package day1;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int sum =
-                Files.lines(Paths.get("input"))
+        Path input = Paths.get("input");
+        int partOneSum =
+                Files.lines(input)
+                        .mapToInt(Main::firstAndLast)
+                        .sum();
+        int partTwoSum =
+                Files.lines(input)
+                        .map(Main::toDigits)
                         .mapToInt(Main::firstAndLast)
                         .sum();
 
-        System.out.println(sum);
+        System.out.printf("answer to part one: %d // part one only asked to give the first and last occurrence of digits " +
+                "%nanswer to part two: %d // part two asked to include the numbers written in words too%n", partOneSum, partTwoSum);
     }
-
-    private static int firstAndLast(String line) {
-        String digits = line.replaceAll("one", "o1e")
+    private static String toDigits(String line) {
+        return line.replaceAll("one", "o1e")
                 .replaceAll("two", "t2o")
                 .replaceAll("three", "t3e")
                 .replaceAll("four", "f4r")
@@ -24,10 +31,12 @@ public class Main {
                 .replaceAll("six", "s6x")
                 .replaceAll("seven", "s7n")
                 .replaceAll("eight", "e8t")
-                .replaceAll("nine", "n9e")
-                .replaceAll("\\D+", "");
+                .replaceAll("nine", "n9e");
+    }
 
-        String firstAndLast = String.valueOf(digits.charAt(0)) + digits.charAt(digits.length() - 1);
+    private static int firstAndLast(String line) {
+        String digits = line.replaceAll("\\D+", "");
+        String firstAndLast = String.format("%c%c", digits.charAt(0), digits.charAt(digits.length() - 1));
         return Integer.parseInt(firstAndLast);
     }
 }
