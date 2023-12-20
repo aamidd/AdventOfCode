581d5b78b1804bb1c8c464c481ce6df17ae123a7
Amid Sanai <sanaiamid@gmail.com>
2023-12-20 16:45:44 +0330

    updated cod

--- src/day4/Main.java
+++ src/day4/Main.java
@@ -6,26 +6,32 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TreeMap;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         int sum =
                 Files.lines(Paths.get("day4input"))
-                        .mapToInt(Main::numberOfMatches)
+                        .mapToInt(Main::cardsWorth)
                         .sum();
         System.out.println(sum);
+    }
+
+    private static int cardsWorth(String line) {
+        long count = numberOfMatches(line);
+        if (count == 0)
+            return 0;
+        return (int) Math.pow(2, count - 1);
     }
 
     private static int numberOfMatches(String line) {
         String card = line.substring(line.indexOf(":") + 2);
         ArrayList<String> ownedNumbers = new ArrayList<>(List.of(card.split("\\|")[1].split(" ")));
 
-        long count = Arrays.stream(card.split("\\|")[0].split(" "))
+        return (int) Arrays.stream(card.split("\\|")[0].split(" "))
                 .filter(number -> !number.isEmpty())
                 .filter(ownedNumbers::contains)
                 .count();
-        if (count == 0)
-            return 0;
-        return (int) Math.pow(2, count - 1);
     }
 }
