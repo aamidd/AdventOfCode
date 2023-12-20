98bd2b347c7bd15a4b3da22c6ba36d1a50e524e0
Amid Sanai <sanaiamid@gmail.com>
2023-12-20 13:45:29 +0330

    part one of day 4

---
+++ src/day4/Main.java
@@ -0,0 +1,31 @@
+package day4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int sum =
+                Files.lines(Paths.get("day4input"))
+                        .mapToInt(Main::worthOfCard)
+                        .sum();
+        System.out.println(sum);
+    }
+
+    private static int worthOfCard(String line) {
+        String card = line.substring(line.indexOf(":") + 2);
+        ArrayList<String> ownedNumbers = new ArrayList<>(List.of(card.split("\\|")[1].split(" ")));
+
+        long streamCount =  Arrays.stream(card.split("\\|")[0].split(" "))
+                .filter(number -> !number.isEmpty())
+                .filter(ownedNumbers::contains)
+                .count();
+        if (streamCount == 0)
+            return 0;
+        return (int) Math.pow(2, streamCount - 1);
+    }
+}
